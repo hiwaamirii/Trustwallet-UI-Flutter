7d0b754274d03a3fad9377596fc866f4e1632eb5
hiwaamirii <hiwaamiri.ir@gmail.com>
2024-01-07 23:28:26 +0330

    Change app bar theme

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -11,22 +11,24 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         elevation: 0,
-        backgroundColor: Colors.black87,
         title: const Text('Home',
           style: TextStyle(
             fontSize: 18,
+            color: Colors.black,
           ),
         ),
         centerTitle: true,
         leading: IconButton(
-          icon: const Icon(Icons.settings, color: Colors.grey),
+          icon: const Icon(Icons.settings, color: Colors.black45),
           onPressed: () {},
         ),
         actions: <Widget>[
           IconButton(
-            icon: const Icon(Icons.circle_notifications_sharp, color: Colors.grey),
+            icon: const Icon(Icons.circle_notifications_sharp, color: Colors.black45),
             onPressed: () {},
           ),
         ],
