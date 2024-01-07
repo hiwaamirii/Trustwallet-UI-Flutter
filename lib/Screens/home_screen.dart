474bd6bc6d8d3e974b961434eb7a036cc40f219a
hiwaamirii <hiwaamiri.ir@gmail.com>
2024-01-07 23:17:11 +0330

    Complete app bar

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -12,10 +12,24 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         backgroundColor: Colors.black87,
-        title: const Text("Home"),
+        title: const Text('Home',
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
         centerTitle: true,
-
+        leading: IconButton(
+          icon: const Icon(Icons.settings, color: Colors.grey),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.circle_notifications_sharp, color: Colors.grey),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: const Column(
         children: [],
