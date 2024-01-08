8f24ea263fee09c42ba12de22c57a60f2d117a35
hiwaamirii <hiwaamiri.ir@gmail.com>
2024-01-08 16:57:23 +0330

    Wallet name + Wallet icons + Balance

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -35,9 +35,9 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: const Column(
-        children: [
-          Row(
+      body: Column(
+        children: <Widget>[
+          const Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SizedBox(
@@ -60,6 +60,93 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ],
           ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.only(right: 12, left: 12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Row(
+                  children: [
+                    Text(
+                      "Code Wallet",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                    Icon(
+                      Icons.arrow_drop_down_sharp,
+                      color: Colors.black45,
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black12,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      width: 30,
+                      height: 30,
+                      child: const Icon(
+                        Icons.file_copy,
+                        color: Colors.black45,
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black12,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      width: 30,
+                      height: 30,
+                      child: const Icon(
+                        Icons.qr_code_scanner_outlined,
+                        color: Colors.black45,
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black12,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      width: 30,
+                      height: 30,
+                      child: const Icon(
+                        Icons.notifications,
+                        color: Colors.black45,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 8),
+          const Padding(
+            padding: EdgeInsets.only(right: 12, left: 12),
+            child: Row(
+              children: [
+                Text(
+                  "\$256.20",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(width: 3),
+                Icon(
+                  Icons.visibility_rounded,
+                  color: Colors.black45,
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
